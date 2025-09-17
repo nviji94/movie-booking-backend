300c7a7219e659444514ed55dd202dc444cbfff1
Vijaya Natarajan <149625816+nviji94@users.noreply.github.com>
2025-09-17 17:53:25 +0200

    Revert "refactor: changed a seperate utils file for baseurl"
    This reverts commit c0fd4be1c5f99963b7dcf334fe3cc692de041937.

--- src/utils/getBaseUrl.ts
+++ src/utils/getBaseUrl.ts
@@ -1,8 +1,7 @@
 export const getBaseUrl = () => {
   if (process.env.NODE_ENV === "production") {
     return (
-      process.env.BACKEND_URL ||
-      "https://movie-booking-backend-m0hk.onrender.com"
+      process.env.BACKEND_URL || "https://movie-booking-backend.onrender.com"
     );
   }
   return `http://localhost:${process.env.PORT || 4000}`;
