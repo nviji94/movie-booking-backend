089133515bf3fae406933db183937d09539d54f6
Vijaya Natarajan <149625816+nviji94@users.noreply.github.com>
2025-09-17 17:53:07 +0200

    Revert "changed the upload middleware"
    This reverts commit f07fa72d7396dc0315141419ee039d04875498ed.

--- src/middleware/upload.middleware.ts
+++ src/middleware/upload.middleware.ts
@@ -1,22 +1,13 @@
 import multer from "multer";
 import path from "path";
-import fs from "fs";
 
 const sanitizeFileName = (name: string) => {
   return name.replace(/\s+/g, "_").replace(/[^a-zA-Z0-9_\-\.]/g, "");
 };
 
-// Absolute path to uploads folder
-const uploadDir = path.join(__dirname, "../uploads");
-
-// Ensure the uploads folder exists
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir, { recursive: true });
-}
-
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, uploadDir);
+    cb(null, "uploads/");
   },
   filename: (req, file, cb) => {
     const ext = path.extname(file.originalname);
